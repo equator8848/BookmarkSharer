bf1800e567dfbe0fea6ff92b678a07154abb01ea
LIBINKAI <841116931@qq.com>
2020-03-31 18:09:45 +0800

    脚本

--- BookmarkSharer/BookmarkSharer/settings_backup.py
+++ BookmarkSharer/BookmarkSharer/settings_backup.py
@@ -42,12 +42,43 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # 跨域中间件，注意顺序
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    '*'
+)
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'VIEW',
+)
+
+CORS_ALLOW_HEADERS = (
+    'XMLHttpRequest',
+    'X_FILENAME',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'Pragma',
+)
 
 ROOT_URLCONF = 'BookmarkSharer.urls'
 
--- push.sh
+++ push.sh
@@ -1,3 +1,17 @@
+# please run this shell in project root dir
+
+last_modified_time=$(stat requirements.txt | sed -n '6p' | awk '{print $2,$3}')
+
+last_modified_timestamp=$(date -d "$last_modified" +"%s")
+
+now_timestamp=$(date +%s)
+
+# seconds
+modified_threshold=3600
+
+if [ $now_timestamp-$last_modified_timestamp -ge $modified_threshold ]; then
+  pip freeze >requirements.txt
+fi
 git add .
 
 git commit -m "$1"
