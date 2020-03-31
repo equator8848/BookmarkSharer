5076b4b5fed468029d678bd821d2f31a45ea496b
LIBINKAI <841116931@qq.com>
2020-03-31 18:23:56 +0800

    脚本�

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-# please run this shell in project root dir
-
 last_modified_time=$(stat requirements.txt | sed -n '6p' | awk '{print $2,$3}')
 
 last_modified_timestamp=$(date -d "$last_modified" +"%s")
@@ -10,12 +8,5 @@ now_timestamp=$(date +%s)
 modified_threshold=3600
 
 if [ $(($now_timestamp-$last_modified_timestamp)) -ge $modified_threshold ]; then
-  echo 'create new requirements.txt ...'
-  pip freeze >requirements.txt
+  echo 'good job !'
 fi
-
-git add .
-
-git commit -m "$1"
-
-git push origin master
